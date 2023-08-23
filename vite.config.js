a8a6a371cf616034ca8bb56e250cfc36129d02e4
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2023-08-23 14:13:48 +0200

    render static index.js for re-usal in engine-cockpit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,16 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   const config = {
-    build: { outDir: 'dist', chunkSizeWarningLimit: 5000 },
+    build: { 
+      outDir: 'dist', 
+      chunkSizeWarningLimit: 5000 ,
+      rollupOptions: {
+        output: {
+          entryFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`
+        }
+      }
+    },
     resolve: {
       alias: { path: 'path-browserify' }
     },
