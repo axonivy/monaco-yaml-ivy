ac51c8b8176132d89f8b7d933e1574dd91a0b9c0
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2023-09-19 11:10:49 +0200

    XIVY-11447 review: simplify build

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,7 @@ export default defineConfig(() => {
   const config = {
     build: { 
       outDir: 'dist', 
-      chunkSizeWarningLimit: 5000 ,
-      rollupOptions: {
-        output: {
-          entryFileNames: `assets/[name].js`,
-          assetFileNames: `assets/[name].[ext]`
-        }
-      }
-    },
-    resolve: {
-      alias: { path: 'path-browserify' }
+      chunkSizeWarningLimit: 5000
     },
     base: './',
   };
